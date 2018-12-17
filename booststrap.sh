819322bf3d92e08566f87ddc202430f3c36efae2
tjonestech <tjones.tech@gmail.com>
2018-12-17 12:44:33 -0500

    Update booststrap.sh

--- booststrap.sh
+++ booststrap.sh
@@ -2,3 +2,13 @@
 
 apt-get update
 apt-get upgrade -y 
+
+sudo apt-get install linux-headers-$(uname -r) build-essential dkms
+
+wget http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
+sudo mkdir /media/VBoxGuestAdditions
+sudo mount -o loop,ro VBoxGuestAdditions_4.3.8.iso /media/VBoxGuestAdditions
+sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
+rm VBoxGuestAdditions_4.3.8.iso
+sudo umount /media/VBoxGuestAdditions
+sudo rmdir /media/VBoxGuestAdditions
